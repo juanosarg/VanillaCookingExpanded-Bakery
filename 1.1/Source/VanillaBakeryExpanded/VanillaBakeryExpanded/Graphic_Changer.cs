87a0e2b246ada107cfdf8489bc732f6f39e26910
juanosarg <31105173+juanosarg@users.noreply.github.com>
2020-08-01 16:51:53 +0200

    Initial commit

---
+++ 1.1/Source/VanillaBakeryExpanded/VanillaBakeryExpanded/Graphic_Changer.cs
@@ -0,0 +1,42 @@
+﻿using RimWorld;
+using Verse;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VanillaBakeryExpanded
+{
+    public class Graphic_Changer: ThingWithComps
+    {
+        public bool doOnce = false;
+
+        public override Graphic Graphic
+        {
+            get
+            {
+                if (!doOnce) {
+                    System.Random rand = new System.Random();
+                    if (rand.NextDouble() > 0.5)
+                    {
+                        //Log.Message("El grafico original");
+                        doOnce = true;
+                        return this.DefaultGraphic;
+                    }
+                    else
+                    {
+                       // Log.Message("El nuevo grafico");
+                        Graphic newgraphic = GraphicDatabase.Get(typeof(Graphic_StackCount), "Things/Meals/VCE_OtherAlternate", this.def.graphicData.shaderType.Shader, this.def.graphicData.drawSize, this.DrawColor, this.DrawColorTwo);
+                        doOnce = true;
+                        return newgraphic;
+
+                    } 
+
+                }else{ return this.DefaultGraphic; }
+                
+              
+            }
+        }
+
+       
+    }
+}
